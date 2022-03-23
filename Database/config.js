842a39cd31d6e672f970e01605fac53797053070
ivanFork <eiom229012@gmail.com>
2022-03-23 09:30:15 -0600

    cors y expres- primer commit

--- Database/config.js
+++ Database/config.js
@@ -1,21 +1,27 @@
-const mongoose = require('mongoose');//importo mongoose
+const mongoose = require('mongoose');
 
-const dbConnection = async ()=>{//creo mi conexion a mongo 
+const dbConnection = async() => {
+
     try {
-        //paso mi cadena de conexion a mongo mediante el archivo de la variables de entonrno process.env
-        await mongoose.connect(process.env.DC_CONNECT,{
-             useNewUrlParser: true,
-             useUnifiedTopology: true,
-            //  useCreateIndex: true,
-         });
-         console.log('bd Online');
+
+    
+     
+
+        await mongoose.connect( process.env.DB_CNN, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true        
+        });
+
+        console.log('DB Online');
         
     } catch (error) {
         console.log(error);
-        throw new Error('Error al conectar a la base de datos');
+        throw new Error('Error a la hora de iniciar la BD ver logs');
     }
+
+
 }
 
-module.exports={//exporto mi conexion para usarla desde otro lugar
+module.exports = {
     dbConnection
 }
--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-require('dotenv').config();//importo mi archivo env o variables de entorno
-const express = require('express');//importo express
-const cors = require('cors');//importo mis cors
+require('dotenv').config();//leo las variables de entorno
+const express = require('express');
 
-const {dbConnection} = require('./Database/config');// importo mi conexion a mongo que esta en Database
+var cors = require('cors');//importo los cors
+const { dbConnection } = require('./database/config');//jalo mi conexion a la base de datos
+//password    dm8hVzxBTGRBHxWD
 
-const app = express();//creo el servidor de express
+// Crear el servidor de express
+const app = express();
 
-//configuracion de cors
+//cors
 app.use(cors());
 
-//lectura y parseo del body recibo del pastman o front 
-app.use(express.json());
-
-
-//base de datos 
-dbConnection();//ejecuto mi conexion a mongo
-
-
-//rutas                 //controlador
-app.use('/api/usuarios',require('./routes/usuarios'));//concatamos toda la ruta desde el controlador
-app.use('/api/hospitales',require('./routes/hospitales'));//concateno toda la ruta desde el controlador
-app.use('/api/medicos',require('./routes/medicos'));//concateno toda la ruta desde el controlador
-app.use('/api/todo',require('./routes/busquedas'));//creo la ruta concatenando el controlador
-app.use('/api/login',require('./routes/auth'));
-app.use('/api/upload',require('./routes/uploads'));
-
-//mediante las variables de entonro process.env
-app.listen(process.env.PORT,()=>{
-    console.log('corriendo el servidor' + ' ' + process.env.PORT);
+dbConnection();//ejecuto mi conexion a db
+app.get('/', (req, res) => {
+ res.json({ 
+     ok: true,
+     msg: 'dfgdgfdg'
+ })
 });
 
-
-
-
-
-
-
-
+app.listen(process.env.PORT, ()=>{
+    console.log('puerto' + 3000);
+});
